131b9f8b8ca4e8eb42297fad760f90784f943bb7
Andres <77512331+Andresmup@users.noreply.github.com>
2024-01-11 19:34:56 -0300

    Add pytorch function

---
+++ engine_sam.py
@@ -0,0 +1,119 @@
+"""
+Contains functions for training and testing a PyTorch model.
+"""
+from typing import Dict, List, Tuple
+
+import torch
+
+from tqdm.auto import tqdm
+
+def train_sam(model,
+              train_dataloader,
+              test_dataloader,
+              optimizer,
+              loss_fn,
+              epochs,
+              device):
+    
+    # Create empty results dictionary
+    results = {"train_loss": [],
+               "train_acc": [],
+               "test_loss": [],
+               "test_acc": []
+    }
+
+    # Loop through training and testing steps for a number of epochs
+    for epoch in tqdm(range(epochs)):
+        train_loss, train_acc = train_step_sam(model=model,
+                                               dataloader=train_dataloader,
+                                               loss_fn=loss_fn,
+                                               optimizer=optimizer,
+                                               device=device)
+        test_loss, test_acc = test_step_sam(model=model,
+                                            dataloader=test_dataloader,
+                                            loss_fn=loss_fn,
+                                            device=device)
+
+        # Print out what's happening
+        print(
+            f"Epoch: {epoch + 1} | "
+            f"train_loss: {train_loss:.4f} | "
+            f"train_acc: {train_acc:.4f} | "
+            f"test_loss: {test_loss:.4f} | "
+            f"test_acc: {test_acc:.4f}"
+        )
+
+        # Update results dictionary
+        results["train_loss"].append(train_loss)
+        results["train_acc"].append(train_acc)
+        results["test_loss"].append(test_loss)
+        results["test_acc"].append(test_acc)
+
+    # Return the filled results at the end of the epochs
+    return results
+
+def train_step_sam(model,
+                   dataloader,
+                   loss_fn,
+                   optimizer,
+                   device):
+    # Put model in train mode
+    model.train()
+
+    # Setup train loss and train accuracy values
+    train_loss, train_acc = 0, 0
+
+    # Loop through data loader data batches
+    for batch, (X, y) in enumerate(dataloader):
+        # Send data to target device
+        X, y = X.to(device), y.to(device)
+
+        # First forward-backward pass
+        loss = loss_fn(model(X), y)
+        loss.backward()
+        optimizer.first_step(zero_grad=True)
+
+        # Second forward-backward pass
+        loss_fn(model(X), y).backward()
+        optimizer.second_step(zero_grad=True)
+
+        # Calculate and accumulate accuracy metric across all batches
+        y_pred_class = torch.argmax(torch.softmax(model(X), dim=1), dim=1)
+        train_acc += (y_pred_class == y).sum().item() / len(y_pred_class)
+
+    # Adjust metrics to get average loss and accuracy per batch
+    train_loss = train_loss / len(dataloader)
+    train_acc = train_acc / len(dataloader)
+    return train_loss, train_acc
+
+def test_step_sam(model,
+                  dataloader,
+                  loss_fn,
+                  device):
+    # Put model in eval mode
+    model.eval()
+
+    # Setup test loss and test accuracy values
+    test_loss, test_acc = 0, 0
+
+    # Loop through DataLoader batches
+    for batch, (X, y) in enumerate(dataloader):
+        # Send data to target device
+        X, y = X.to(device), y.to(device)
+
+        # First forward-backward pass
+        loss = loss_fn(model(X), y)
+        optimizer.first_step(zero_grad=True)
+
+        # Second forward-backward pass
+        loss_fn(model(X), y).backward()
+        optimizer.second_step(zero_grad=True)
+
+        # Calculate and accumulate accuracy
+        test_pred_labels = torch.argmax(torch.softmax(model(X), dim=1), dim=1)
+        test_acc += ((test_pred_labels == y).sum().item() / len(test_pred_labels))
+
+    # Adjust metrics to get average loss and accuracy per batch
+    test_loss = test_loss / len(dataloader)
+    test_acc = test_acc / len(dataloader)
+    return test_loss, test_acc
---
+++ train_model_sam.py
@@ -0,0 +1,79 @@
+import os
+import torch
+from torch.utils.data import DataLoader
+from timeit import default_timer as timer
+
+try:
+  from sam import SAM
+except ImportError:
+    !pip install sam-pytorch
+    from sam import SAM
+
+try:
+    from pytorch_functions import engine, utils
+except ImportError:
+    os.system("git clone https://github.com/Andresmup/pytorch_functions")
+    from pytorch_functions import engine, utils
+
+
+def train_model_sam(model,
+                    train_dataloader,
+                    test_dataloader,
+                    model_save_name,
+                    base_optimizer=torch.optim.Adam,
+                    lr=0.001,
+                    momentum=0.9,
+                    NUM_EPOCHS=5):
+
+    """
+    Trains a PyTorch image classification model with SAM.
+
+    Args:
+        model: PyTorch model to be trained.
+        train_dataloader: torch DataLoaders with training data.
+        test_dataloader: torch DataLoaders with testing data.
+        model_save_name: Name to save the trained model.
+        optimizer: PyTorch optimizer (optional, default is None).
+        NUM_EPOCHS: Number of training epochs (default is 5).
+        LEARNING_RATE: Learning rate for the optimizer (default is 0.001).
+    """
+    # Set random seeds
+    torch.manual_seed(42)
+    torch.cuda.manual_seed(42)
+
+    # Setup target device
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    # Set loss function
+    loss_fn = torch.nn.CrossEntropyLoss()
+
+    # Create SAM optimizer
+    base_optimizer = base_optimizer
+    optimizer = SAM(model.parameters(), base_optimizer, lr=lr, momentum=momentum)
+
+    # Start the timer
+    start_time = timer()
+
+    # Train model with SAM
+    results = train_sam(
+        model=model,
+        train_dataloader=train_dataloader,
+        test_dataloader=test_dataloader,
+        optimizer=optimizer,
+        loss_fn=loss_fn,
+        epochs=NUM_EPOCHS,
+        device=device
+    )
+
+    # End the timer and print out how long it took
+    end_time = timer()
+    execution_time = end_time - start_time
+    print(f"[INFO] Total training time: {execution_time:.3f} seconds")
+
+    # Save the model
+    utils.save_model(
+        model=model,
+        target_dir="models",
+        model_name=model_save_name
+    )
+    return results, execution_time
